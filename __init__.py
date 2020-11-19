e302eccd3ed7e5521e0ad087d3ee8391bca3af74
ryanxing <xingql@buaa.edu.cn>
2020-11-19 23:04:30 +0800

    Update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .conversion import bgr2rgb, rgb2bgr, dict2str
 from .dataset import (
-    DiskIODataset, LMDBIODataset, DistSampler, create_dataloader,
+    DistSampler, create_dataloader,
     CPUPrefetcher,
     )
 from .deep_learning import init_dist
@@ -10,7 +10,7 @@ from .system import get_timestr, arg2dict, mkdir_archived, set_random_seed
 __all__ = [
     'bgr2rgb', 'rgb2bgr', 'dict2str',
     ] + [
-    'DiskIODataset', 'LMDBIODataset', 'DistSampler', 'create_dataloader',
+    'DistSampler', 'create_dataloader',
     'CPUPrefetcher',
     ] + [
     'init_dist',
