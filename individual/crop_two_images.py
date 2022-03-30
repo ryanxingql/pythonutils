a0564de86e6ed82ada82a4fdfa4f4fd432639fb9
Qunliang Xing <ryanxingql@gmail.com>
2022-03-30 15:18:37 +0800

    polish

--- individual/crop_two_images.py
+++ individual/crop_two_images.py
@@ -4,6 +4,8 @@ python crop_two_images.py -img1 <img1-path> -img2 <img2-path>
 from cv2 import cv2
 import numpy as np
 import argparse
+import os.path as osp
+import os
 
 
 def add_red_box(img, start_h, start_w, h, w):
@@ -42,11 +44,15 @@ def show_two_imgs(img1, img2, cap1='img1', cap2='img2'):
 parser = argparse.ArgumentParser()
 parser.add_argument('-img1', type=str)
 parser.add_argument('-img2', type=str)
+parser.add_argument('-save_dir', type=str, default='log')
 parser.add_argument('-start_h', type=int, default=0)
 parser.add_argument('-start_w', type=int, default=0)
 parser.add_argument('-crop_h', type=int, default=100)
 parser.add_argument('-crop_w', type=int, default=100)
 args = parser.parse_args()
+
+if not osp.exists(args.save_dir):
+	os.mkdir(args.save_dir)
 
 # read images
 img1 = cv2.imread(args.img1)
@@ -95,10 +101,11 @@ while if_first or (if_ok != 'y'):
 
 img1_patch = img1[start_h:start_h+crop_h, start_w:start_w+crop_w, :]
 img2_patch = img2[start_h:start_h+crop_h, start_w:start_w+crop_w, :]
-img_ori_name = args.img1.split('/')[-1].split('.')[0]
-img1_save_name = img_ori_name + f'_{start_h}_{start_w}_{crop_h}_{crop_w}_img1.png'
-img2_save_name = img_ori_name + f'_{start_h}_{start_w}_{crop_h}_{crop_w}_img2.png'
-cv2.imwrite(img1_save_name, img1_patch)
-cv2.imwrite(img2_save_name, img2_patch)
-print(f'img1 saved at {img1_save_name}')
-print(f'img2 saved at {img2_save_name}')
+img1_ori_name = args.img1.split('/')[-1].split('.')[0]
+img2_ori_name = args.img2.split('/')[-1].split('.')[0]
+img1_save_path = osp.join(args.save_dir, img1_ori_name + f'_{start_h}_{start_w}_{crop_h}_{crop_w}_img1.png')
+img2_save_path = osp.join(args.save_dir, img2_ori_name + f'_{start_h}_{start_w}_{crop_h}_{crop_w}_img2.png')
+cv2.imwrite(img1_save_path, img1_patch)
+cv2.imwrite(img2_save_path, img2_patch)
+print(f'img1 saved at {img1_save_path}')
+print(f'img2 saved at {img2_save_path}')
